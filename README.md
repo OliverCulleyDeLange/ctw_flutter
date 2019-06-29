7913e481534fc62d2dffabfd9e57e7c37614656b
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-06-29 12:37:50 +0100

    Initial commit - sample flutter project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cheat to Win
+
+A Flutter game app.
+
+### Challenges (and how you cheat)
+- Quiz questions / Riddles - (google the answer)
+- Be in a location - (fake GPS)
+- Quiz - Google it
+- Hidden menu to complete a level, down like 100 layers
+- Easter eggs like long pressing buttons, click X times
+- Mini games that seem like you win one way but you can cheat
+    - Maze game - drag and drop the player, no need to complete the maze
+- Totally random score for completing a level.?
+- What's the password? Random 4 letter word that changes every day. Or maybe can be found in the some where? Or maybe fingerprint?
+- Device challenge like set a pin or password
+- Answer hidden in image so you have to tilt phone
+
+
+
+Monetisation / Propagation:
+- Watch ad for hints / clues
+- Pay to win - buy me a beer level. (If you enjoyed the app)
+- Share link with a friend who has to download the app and complete the game before they get a point
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_title)),
+        body: MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+      return Center(
+        child: Text(
+          'Item $index',
+          style: Theme.of(context).textTheme.headline,
+        ),
+      );
+    })
+    );
+  }
+}
