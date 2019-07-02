3a077528295d24de79d14d90a1796aebc61deed3
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-07-02 23:21:59 +0100

    Animate color change of a single tile

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:ctw_flutter/challenges.dart';
+import 'package:ctw_flutter/screens/widgets/challenge-tile.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,53 +7,14 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<Color> _colorTween;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 3),
-      vsync: this,
-    );
-    _colorTween = ColorTween(
-      begin: Colors.deepOrange,
-      end: Colors.green,
-    ).animate(_controller);
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
         crossAxisCount: 4,
-        children: List.generate(challenges.length, (index) {
-          return InkWell(
-              onTap: () async {
-                var won = await Navigator.pushNamed(context, "$index");
-                print("Challenge result: $won");
-                if (_controller.status == AnimationStatus.completed) {
-                  _controller.reverse();
-                } else {
-                  _controller.forward();
-                }
-              },
-              child: AnimatedBuilder(
-                animation: _colorTween,
-                builder: (context, child) {
-                  return Card(
-                    color: _colorTween.value,
-                    child: Text(
-                      index.toString(),
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .body1,
-                    ),
-                  );
-                },
-              ));
-        }));
+        children: List.generate(challenges.length, (index) =>
+            ChallengeTile(index)
+        ));
   }
 }
+
---
+++ lib/screens/widgets/challenge-tile.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ChallengeTile extends StatefulWidget {
+  final index;
+
+  ChallengeTile(this.index);
+
+  @override
+  _ChallengeTileState createState() => _ChallengeTileState();
+}
+
+class _ChallengeTileState extends State<ChallengeTile>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Color> _colorTween;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _colorTween = ColorTween(
+      begin: Colors.deepOrange,
+      end: Colors.green,
+    ).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () async {
+          var won = await Navigator.pushNamed(context, "${widget.index}");
+          print("Challenge result: $won");
+          _controller.forward();
+        },
+        child: AnimatedBuilder(
+          animation: _colorTween,
+          builder: (context, child) {
+            return Card(
+              color: _colorTween.value,
+              child: Text(
+                widget.index.toString(),
+                style: Theme.of(context).textTheme.body1,
+              ),
+            );
+          },
+        ));
+  }
+}
