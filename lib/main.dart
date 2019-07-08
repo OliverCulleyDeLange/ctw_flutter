b5ed18aafa79b4b4deb0f2e5d64d8cf09b6297ef
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-07-08 20:47:18 +0100

    Fix restart widget position

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,18 @@ import 'package:ctw_flutter/ui/widgets/restart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(RestartWidget(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RestartWidget(
-      child: MaterialApp(
-        title: "Cheat to win",
-        theme: getTheme(),
-        home: Scaffold(
-          body: ChangeNotifierProvider(
-            builder: (context) => HomeModel(),
-            child: Home(),
-          ),
+    return MaterialApp(
+      title: "Cheat to win",
+      theme: getTheme(),
+      home: Scaffold(
+        body: ChangeNotifierProvider(
+          builder: (context) => HomeModel(),
+          child: Home(),
         ),
       ),
     );
