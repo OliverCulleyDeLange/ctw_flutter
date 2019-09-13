e6bafad723bd55017b2f3f6e9e91b7d2a8c857e3
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-09-13 20:26:26 +0100

    Maze but with flame game engine

--- lib/ui/challenges/game/maze.dart
+++ lib/ui/challenges/game/maze.dart
@@ -1,89 +1,115 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:flame/components/component.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 
-class Maze extends StatefulWidget {
-  @override
-  _MazeState createState() => _MazeState();
-}
-
-class _MazeState extends State<Maze> with SingleTickerProviderStateMixin {
-  AnimationController _animation;
-  StreamSubscription<AccelerometerEvent> accelerometer;
-
-//  GyroscopeEvent gyro;
-  AccelerometerEvent accelEvent;
-
-  @override
-  void initState() {
-    super.initState();
-//    gyro = GyroscopeEvent(0, 0, 0);
-    accelEvent = AccelerometerEvent(0, 0, 0);
-    accelerometer = accelerometerEvents.listen((AccelerometerEvent event) {
-//      debugPrint("Acceleromter: ${event.x.toStringAsPrecision(2)},${event.y.toStringAsPrecision(2)}");
-      setState(() {
-        accelEvent = event;
-      });
-    });
-//    gyroscopeEvents.listen((GyroscopeEvent event) {
-//      debugPrint("Gyroscope:    ${event.x.toStringAsPrecision(2)},${event.y.toStringAsPrecision(2)}");
-//      setState(() {
-//        gyro = event;
-//      });
-//    });
-
-    _animation = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 5),
-    );
-    _animation.repeat();
-  }
-
-  @override
-  void dispose() {
-    accelerometer.cancel();
-    _animation.dispose();
-    super.dispose();
-  }
-
+class Maze extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    return RotationTransition(
-//      child:
-    return CustomPaint(
-      size: Size.infinite,
-      painter: MazePainter(accelEvent),
-    ); //,
-//      turns: _animation,
-//    );
+    return MazeGame().widget;
   }
 }
 
-class MazePainter extends CustomPainter {
-  double circleRadius = 50;
+class MazeGame extends BaseGame {
+  StreamSubscription<AccelerometerEvent> accelerometer;
 
-//  GyroscopeEvent pos;
-  AccelerometerEvent pos;
+  Player player;
 
-  MazePainter(this.pos);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.lightBlueAccent[400]
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 20;
-//    canvas.drawLine(Offset(0, 0), Offset(size.width, size.height), paint);
-    var midX = size.width / 2;
-    var midY = size.height / 2;
-    canvas.drawCircle(
-        Offset((-pos.x * midX / 2) + midX, (pos.y * midY / 2) + midY),
-        circleRadius,
-        paint);
+  MazeGame() {
+    player = Player(accelerometer);
+    add(player); // this will call resize the first time as well
   }
 
   @override
-  bool shouldRepaint(MazePainter old) => old.pos != pos;
+  void onAttach() {
+    debugPrint("Game attach");
+    accelerometer = accelerometerEvents.listen(player.accelerometerListener);
+  }
+
+  @override
+  void onDetach() {
+    debugPrint("Game detatch");
+    accelerometer.cancel();
+  }
+
+  @override
+  void lifecycleStateChange(AppLifecycleState state) {
+    debugPrint("Game state now: $state");
+  }
 }
+
+class Player extends PositionComponent {
+  final paint = Paint()
+    ..color = Colors.lightBlueAccent[400]
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 20;
+
+  Size canvassSize;
+  double maxXLean = 3.0;
+  double maxYLean = 1.5;
+  double xmod = 0.0;
+  double ymod = 0.0;
+  double speed = 0.0;
+
+  AccelerometerEvent latestAccelerometerEvent = AccelerometerEvent(0, 0, 0);
+
+  Player(StreamSubscription<AccelerometerEvent> accelerometer);
+
+  void Function(AccelerometerEvent event) get accelerometerListener =>
+          (AccelerometerEvent event) {
+//        debugPrint("Acceleromter: ${event.x.toStringAsPrecision(2)},${event.y.toStringAsPrecision(2)}");
+        latestAccelerometerEvent = event;
+      };
+
+  @override
+  void render(Canvas canvas) {
+    prepareCanvas(canvas);
+    // 0,0 used as canvas is positioned
+    canvas.drawCircle(Offset(xmod, ymod), 20, paint);
+  }
+
+  @override
+  void update(double t) {
+//    debugPrint("$xmod $ymod ${latestAccelerometerEvent.toString()}");
+    var newXmod = xmod + latestAccelerometerEvent.x;
+    var newYmod = ymod + latestAccelerometerEvent.y;
+    if (newXmod.abs() < canvassSize.width / 2) {
+      xmod = newXmod;
+    }
+    if (newYmod.abs() < canvassSize.height / 2) {
+      ymod = newYmod;
+    }
+  }
+
+  @override
+  void resize(Size size) {
+    debugPrint("Player resize");
+    canvassSize = size;
+    this.x = (size.width - this.width) / 2;
+    this.y = (size.height - this.height) / 2;
+  }
+}
+
+//class MazePainter extends CustomPainter {
+//  double circleRadius = 50;
+//  Offset pos;
+//
+//  MazePainter(this.pos);
+//
+//  @override
+//  void paint(Canvas canvas, Size size) {
+//
+//    var midX = size.width / 2;
+//    var midY = size.height / 2;
+//    canvas.drawCircle(
+//        Offset((pos.dx * size.width) + midX, (pos.dy * size.height) + midY),
+//        circleRadius,
+//        paint);
+//  }
+//
+//  @override
+//  bool shouldRepaint(MazePainter old) => false;
+//}
