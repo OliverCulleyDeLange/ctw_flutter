bde3b99c1f0bc886ae8d547b7de7f0a749c54666
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2019-09-23 13:59:44 +0100

    Added Hints

--- lib/ui/challenges/hints.dart
+++ lib/ui/challenges/hints.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 
 class Hints {
   static Map<String, List<Widget>> _hints = {
-    "single-tap": [Text("Tap"), Text("Tap2"), Text("Tap3"), Text("Tap4")],
+    "double-tap": [Text("Double the previous challenge")],
+    "long-press": [Text("Patience")],
+    "drag-and-drop": [Text("Always sort your recyling")],
+    "rotate": [Text("Locked in portrait mode?")],
+    "shake": [Text("... Rattle and roll")],
+    "passcode": [Text("Take a step back"), Text("Is the code unlocked yet?")],
+    "hidden-word": [Text("3, 2, 1, Blast off."), Text("Turn your brightness up")],
+    "sort": [Text("Oppinions, not fact")],
+    "battery": [Text("Juice please")],
+    "spring": [Text("Opposites attract")],
   };
 
   static Widget get(String name, int hintNumber) {
